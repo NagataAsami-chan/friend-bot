938191ed406501da8c4462ff297664cebef5eb19
Sleeping_A <73869314+ALLVIS-II@users.noreply.github.com>
2022-06-11 00:57:39 +1200

    Update main.js

--- main.js
+++ main.js
@@ -24,25 +24,20 @@ client.on('message', message =>{
         message.channel.send('ping')
     };
     if (command == 'hello') {
-        message.channel.send('nice to meet you')
+        message.channel.send('nice to meet you How are you today')
     }
+    if (command == 'Im Fine') {
+        message.channel.send('Nice')
+    }
+    
     if (command == 'bye') {
         message.channel.send('Stay with me please')
     }
     if (command == 'thankyou') {
         message.channel.send('No worries')
     }
-    if (command == 'DickIs') {
-        message.channel.send(' gay')
-    }
     if (command == 'me') {
         message.channel.send('I am here How can I help you')
-    }
-    if (command == 'n') {
-        message.channel.send('nice')
-    }
-    if (command == 'g') {
-        message.channel.send('gg')
     }
     
     if (command == 'lazy') {
