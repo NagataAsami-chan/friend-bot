c63f49edf93d9b44135125a2573641b4cd992d60
NagataAsami-chan <80683081+NagataAsami-chan@users.noreply.github.com>
2022-06-15 15:00:52 +0700

    Update music.js

--- command/music.js
+++ command/music.js
@@ -1,53 +1,27 @@
-const { Intents, Client } = require('discord.js')
-const play = require('play-dl');
-const { createAudioPlayer, createAudioResource , StreamType, demuxProbe, joinVoiceChannel, NoSubscriberBehavior, AudioPlayerStatus, VoiceConnectionStatus, getVoiceConnection } = require('@discordjs/voice')
+
+const ytdl = require('ytdl-core-discord');
 const ytSearch = require('yt-search');
 const { VoiceChannel } = require('discord.js');
 
 module.exports = {
     name: 'play',
     async execute(message, args) {
-        // const vc = message.member.voice.channel;
-        // if (!args.length) return message.channel.send('???')
+        const vc = message.member.voice.channel;
+        if (!args.length) return message.channel.send('???')
 
-        // const findMusic = async (query) => {
-        //     const result = await ytSearch (query) ;
-        //     return (result.videos.length > 1) ? result.videos[0] : null
-        // }
-        // const music = await findMusic(args.join(' '))
+        const findMusic = async (query) => {
+            const result = await ytSearch (query) ;
+            return (result.videos.length > 1) ? result.videos[0] : null
+        }
+        const music = await findMusic(args.join(' '))
 
-        // const connection = await vc.join()
+        const connection = await vc.join()
 
-        // if (music) {
-        //     const stream = ytdl(music.url, {filter: 'audioonly'})
-        //     connection.play(await stream, {type: 'opus'})
+        if (music) {
+            const stream = ytdl(music.url, {filter: 'audioonly'})
+            connection.play(await stream, {type: 'opus'})
             
-        //     await message.reply(`Now playing ${music.title}`)
-        // } else message.channel.send('no video found')
-        if (!message.member.voice?.channel) return message.channel.send('Connect to a Voice Channel')
-        const connection = joinVoiceChannel({
-            channelId: message.member.voice.channel.id,
-            guildId: message.guild.id,
-            adapterCreator: message.guild.voiceAdapterCreator
-        })
-
-        let yt_info = await play.search(args, {
-            limit: 1
-        })
-        
-        let stream = await play.stream(yt_info[0].url)
-        
-        let resource = createAudioResource(stream.stream, {
-            inputType: stream.type
-        })
-        let player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Play
-            }
-        })
-
-        
-        player.play(resource)
-        connection.subscribe(player)
+            await message.reply(`Now playing ${music.title}`)
+        } else message.channel.send('no video found')
         }
     }
